f5f7931b98c65f33ed95a8c75b7e8af0169c2b30
Noah Pinales <pinales@usc.edu>
2024-04-29 17:43:23 -0700

    brush engine work. scattering & spacing is working

--- src/scripts/tools/Brush.js
+++ src/scripts/tools/Brush.js
@@ -8,67 +8,56 @@ export class Brush extends Tool {
 		super();
 		this.name = "Brush";
 		this.pE = null;
-		this.brushCel = new Cel(2, 2, COLORS.black);
+		this.brushCel = new Cel(1, 1, COLORS.black);
+		this.layerCel = null;
+		this.dummyCel = new Cel(1, 1, COLORS.red);
 	}
 	inputActive(e) {
 		let coords = CoreRenderer.getCoordinatesFromInputEvent(e);
 		let pCoords = CoreRenderer.getCoordinatesFromInputEvent(this.pE);
 		let layer = LayerManager.getActiveLayer();
-		let layerCel = layer.layerCel;
-		let linePoints = line(new Point(coords.x, coords.y), new Point(pCoords.x, pCoords.y));
-		let spacing = -1000;
-		if (spacing >= 0) spacing += 1;
-		if(spacing < 0) spacing = Math.abs(1 / spacing);
-		console.log(spacing);
-		let scattering = 10;
-		for(let ix = 0; ix < linePoints.length; ix += spacing) {
-			let i = Math.floor(ix);
-			let p = linePoints[i];
-			p.x += Math.round((Math.random() * scattering) - (scattering / 2))
-			p.y += Math.round((Math.random() * scattering) - (scattering / 2))
-			layerCel.drawCel(this.brushCel, p.x, p.y, true);
-			
-		}
-        this.pE = e;
+		this.layerCel = layer.layerCel;
+		let linePoints = line(
+			new Point(coords.x, coords.y),
+			new Point(pCoords.x, pCoords.y)
+		);
+		this.brushEngine(linePoints);
+		this.layerCel.drawLine(
+			this.dummyCel,
+			new Point(coords.x, coords.y),
+			new Point(pCoords.x, pCoords.y)
+		);
+		this.pE = e;
 	}
 	inputStart(e) {
 		this.pE = e;
 		let coords = CoreRenderer.getCoordinatesFromInputEvent(e);
 		let layer = LayerManager.getActiveLayer();
-		let layerCel = layer.layerCel;
+		this.layerCel = layer.layerCel;
 		let dummyCel = new Cel(4, 4, COLORS.green);
-		layerCel.drawCel(dummyCel, coords.x, coords.y, true);
-
-		this.brushCel.fill(COLORS.random())
-
-		
-		let spacing = -100000;
-		if (spacing >= 0) spacing += 1;
-		if(spacing < 0) spacing = Math.abs(1 / spacing);
-		console.log(spacing);
-		let scattering = 10;
-		let pp = {...coords};
-		let p = coords;
-		let ix = 0;
-
-		setInterval(() => {
-			if(ix > 1) return;
-			ix += spacing;
-			let i = Math.floor(ix);
-			p.x += Math.round((Math.random() * scattering) - (scattering / 2))
-			p.y += Math.round((Math.random() * scattering) - (scattering / 2))
-			layerCel.drawLine(this.brushCel, p, pp, true);
-			pp = {...p};
-		}, 0.5);
+		this.brushEngine([coords]);
 	}
-	inputEnd(e) {
-
-    }
-    inputPreview(e) {
+	inputEnd(e) {}
+	inputPreview(e) {
 		let coords = CoreRenderer.getCoordinatesFromInputEvent(e);
 		let layer = LayerManager.getActiveLayer();
 		let layerCel = layer.layerCel;
 		let dummyCel = new Cel(4, 4, COLORS.green);
 		//layerCel.tempDrawCel(dummyCel, coords.x, coords.y, true);
-    }
+	}
+	brushEngine(points) {
+		let spacing = -100;
+		if (spacing >= 0) spacing += 1;
+		if (spacing < 0) spacing = Math.abs(1 / spacing);
+		let scattering = 4;
+		for (let ix = 0; ix < points.length; ix += spacing) {
+			let i = Math.floor(ix);
+			let p = points[i];
+			let r = scattering * Math.sqrt(Math.random());
+			let theta = Math.random() * 2 * Math.PI;
+			let dx = Math.round(r * Math.cos(theta));
+			let dy = Math.round(r * Math.sin(theta));
+			this.layerCel.drawCel(this.brushCel, p.x + dx, p.y + dy, true);
+		}
+	}
 }
